4225c2937bc698b2702894270b4aa680e98f3395
Liyons Varghese <liyonsvarghese@gmail.com>
2025-08-04 10:47:06 +0530

    Skeleton

---
+++ src/test/java/com/lvrgese/news_aggregator/NewsAggregatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lvrgese.news_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsAggregatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
